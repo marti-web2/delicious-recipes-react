56a92b425db26d6bb40a5ec5e5cd10db4ce9b60a
David Martinez <anddagger@gmail.com>
2022-06-08 22:59:56 -0500

    added splide slides && styled divs to Popular Picks section of Home page

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,20 +1,100 @@
 import { useEffect, useState } from "react"
+import styled from "styled-components"
+import { Splide, SplideSlide } from "@splidejs/react-splide"
+import "@splidejs/splide/dist/css/splide.min.css"
 
 const Popular = () => {
   const [popular, setPopular] = useState([])
 
   useEffect(_ => {
     getPopular()
-  },[])
+  }, [])
+
   const getPopular = async () => {
     const api = await fetch(
       `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     )
     const data = await api.json()
     setPopular(data.recipes)
+    console.log(popular)
   }
 
-  return <div>Popular</div>
+  const popularRecipes = popular.map((recipe) => (
+    <SplideSlide key={recipe.id}>
+      <Card>
+        <p>{recipe.title}</p>
+        <img src={recipe.image} alt={recipe.title} />
+      </Card>
+    </SplideSlide>
+  ))
+
+  return (
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            arrows: false,
+            drag: "free",
+            gap: "5rem",
+            pagination: false,
+            perPage: 4,
+            speed: 25,
+            waitForTransition: true,
+            wheel: true
+          }}
+        >
+          {popularRecipes}
+        </Splide>
+      </Wrapper>
+    </div>
+  )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0;
+`
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index:10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Gradient = styled.div`
+  z-index: 3;
+  postion: absoulute;
+  width: 100%;
+  height: 100%;
+  background-color: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))
+`
 
 export default Popular
